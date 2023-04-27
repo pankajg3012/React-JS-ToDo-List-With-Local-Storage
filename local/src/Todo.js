aead68d085c61bbbf058f5539606399845ac7b01
pankajg3012 <pankajg3015@gmail.com>
2023-04-27 14:37:12 +0530

    Small Changes

--- local/src/Todo.js
+++ local/src/Todo.js
@@ -6,71 +6,69 @@ function Todo() {
   const [fieldarray, setFieldarray] = useState([]);
 
   const handlechange = (e) => {
-    setInputtext(e.target.value);
-    
 
+    setInputtext(e.target.value);
   };
+
+
+
+  
   const addbtn = () => {
+    if (!inputtext) {
+      alert("invlid");
+      return;
+    }
     // console.log("hit on Add button");
     const list = [...fieldarray, inputtext];
     setFieldarray(list);
 
-    localStorage.setItem('list',JSON.stringify(list));
+    localStorage.setItem("list", JSON.stringify(list));
 
     setInputtext("");
   };
-  const rembtn = (index) =>{
-    setFieldarray((list) => list.filter((l, i) => i !== index ));
-  }
-
+  const rembtn = (index) => {
+    let list = fieldarray.filter((l, i) => i !== index);
+    setFieldarray(list);
+    localStorage.setItem("list", JSON.stringify(list));
+  };
 
   useEffect(() => {
-    console.log("jhghjf")
-    const items = JSON.parse(localStorage.getItem('list'));
-  
-setFieldarray(items ? items : []);
-  
-    }, []);
-
-
-
+    console.log("jhghjf");
+    const items = JSON.parse(localStorage.getItem("list"));
+    setFieldarray(items ? items : []);
+  }, []);
 
   return (
     <div>
       <h1>TO-Do Task List</h1>
       <div className="parent">
-      <div className="child">
-
-        <input
-          type="text"
-          id="feild"
-          value={inputtext}
-          onChange={handlechange}
-          placeholder="Enter Your Task Here......"
-        />
-        <button id="click" onClick={addbtn}>
-          +
-        </button>
+        <div className="child">
+          <input
+            type="text"
+            id="feild"
+            value={inputtext}
+            onChange={handlechange}
+            placeholder="Enter Your Task Here......"
+          />
+          <button id="click" onClick={addbtn}>
+            +
+          </button>
         </div>
         <div className="Additem">
-        <ol>
-       
-        {
-          fieldarray.map((listitem,index)=>
-          (
-            <li key={index}>{listitem}<i className="fa-sharp fa-solid fa-trash Icon" onClick={() => rembtn(index)}></i></li>)
-          
-        
-  )}
-  
-    </ol>
-        
+          <ol>
+            {fieldarray.map((listitem, index) => (
+              <li key={index}>
+                {listitem}
+                <i
+                  className="fa-sharp fa-solid fa-trash Icon"
+                  onClick={() => rembtn(index)}
+                ></i>
+              </li>
+            ))}
+          </ol>
         </div>
-       
       </div>
-    
-    
     </div>
-  )
-            }
+  );
+}
 export default Todo;
